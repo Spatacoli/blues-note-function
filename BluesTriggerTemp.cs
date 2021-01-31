247dcafadebf0efc4805b24cb4e7bcb5b3efdf79
Todd Spatafore <todd.spatafore@vudu.com>
2021-01-31 14:32:16 -0800

    Initial commit

---
+++ BluesTriggerTemp.cs
@@ -0,0 +1,50 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Spatacoli.Blues
+{
+    public static class BluesTriggerTemp
+    {
+        [FunctionName("BluesTriggerTemp")]
+        [return: Table("BluesYouTubeData")]
+        public static async Task<RowData> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var data = JsonConvert.DeserializeObject<TempData>(requestBody);
+
+            string responseMessage = $"Temp is {data.value} and the calibration is {data.calibration}";
+            log.LogInformation(responseMessage);
+            return new RowData 
+            { 
+                PartitionKey = "Http",
+                RowKey = Guid.NewGuid().ToString(),
+                Temperature = data.value,
+                Calibration = data.calibration
+            };
+        }
+    }
+
+    public class RowData
+    {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public double Temperature { get; set; }
+        public double Calibration { get; set; }
+    }
+
+    public class TempData
+    {
+        public double value { get; set; }
+        public double calibration { get; set; }
+    }
+}
